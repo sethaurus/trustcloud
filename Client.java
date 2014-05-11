e7f2d252133e46b8851a5ddb1714c4bb13e24009
Thomas Drake-Brockman <thom@sfedb.com>
2014-05-12 03:05:09 +0800

    Added basic shell Client and Server classes.

---
+++ Client.java
@@ -0,0 +1,6 @@
+class Client {
+  public static void main(String[] args) {
+    System.out.println("TrustCloud Client");
+    System.out.println("CITS3002 - Thomas Drake-Brockman (21150739)");
+  }
+}
